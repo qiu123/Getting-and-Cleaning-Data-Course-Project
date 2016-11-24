b1c3093eef19d74cf3410bbb167d8add1aa535d2
邱心培 <1147664435@qq.com>
2016-11-24 11:16:00 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -7,7 +7,6 @@ library(plyr)
   fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   download.file(fileUrl,destfile="./data/Dataset.zip")
 
-
   # 0.2 Unzip dataSet to /data directory
   unzip(zipfile="./data/Dataset.zip",exdir="./data")
 
